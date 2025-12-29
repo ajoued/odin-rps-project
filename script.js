7786f7e258150e2b54653b4e0672ee6f2d0e1bf9
ajoued <ajoueds@gmail.com>
2025-12-29 11:44:15 +0100

    made the game playable in terminal using node

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import readline from "readline";
-
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 function getComputerChoice() {
   let a = Math.random();
   a = a * 3 + 1;
@@ -13,7 +13,48 @@ function getComputerChoice() {
       return "scissors";
   }
 }
-let a = readline();
-let b = getComputerChoice();
 
-console.log(b);
+function playRound(computerChoice, playerChoice) {
+  computerChoice = computerChoice.toLowerCase();
+  playerChoice = playerChoice.toLowerCase();
+
+  switch (true) {
+    case playerChoice === computerChoice:
+      console.log("it's a tie!");
+      return 0;
+    case playerChoice === "rock" && computerChoice === "scissors":
+      console.log("Player wins");
+      return 1;
+    case playerChoice === "paper" && computerChoice === "rock":
+      console.log("Player wins");
+      return 1;
+    case playerChoice === "scissors" && computerChoice === "paper":
+      console.log("Player wins");
+      return 1;
+
+    default:
+      console.log("Computer wins!");
+      return 2;
+  }
+}
+
+let computerHearts = 5;
+let playerHearts = 5;
+
+while (computerHearts != 0 && playerHearts != 0) {
+  let b = getComputerChoice();
+  const rl = readline.createInterface({ input, output });
+  const a = await rl.question("Enter your answer: ");
+  rl.close();
+
+  let result = playRound(b, a);
+  if (result === 1) {
+    computerHearts--;
+  }
+  if (result === 2) {
+    playerHearts--;
+  }
+
+  console.log(`Player hearts ${playerHearts}`);
+  console.log(`Computer hearts ${computerHearts}`);
+}
